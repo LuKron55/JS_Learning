fe100ff501f42eb2102dc9f1a287710c044c37d7
lukron55 <lukron55@outlook.com>
2024-10-11 20:14:56 +0100

    test

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,20 @@
-const calcArea = (radius) => {
-    return 3.14 * radius**2;
+
+let user = {
+    name: 'harry',
+    age: 17,
+    email: 'harry@harry.men',
+    location: 'clacton',
+    blogs: ['how my hard drive broke', 'the sopranos killed my drive'],
+    login: function(){
+        console.log('harry is gay lmfao')
+    },
+    logout: function(){
+        console.log('harry is not gay lmfao')
+    },
+    logBlogs: function(){
+
+    }
 };
 
-const area = calcArea(5);
-console.log(area)
+
 
